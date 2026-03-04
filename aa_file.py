9761d380dee6ee1722a5f2f9885785cfbb035c88
egg <zmsanoy@carc.pshs.edu.ph>
2026-03-04 14:09:22 +0800

    Update aa_file.py
    committed by ME

--- aa_file.py
+++ aa_file.py
@@ -15,68 +15,22 @@
 #=========================================================================================
 
 # Initialize variables and lists
+import json
+
+filename = 'tipidtracker_data.json'
+
+with open(filename, 'r') as file:
+    data = json.load(file)
+
 index = 0
-total_cost = 0
-date_list_allowance = []
-date_list_expense = []
-allowance_list = []
-cost_list = []
-item_list = []
-remainder = 0
-allowance_total = 0
-total = 0
-
-#==========================
-#Function: Add Allowance
-#==========================
-def add_allowance(user_input, allowance_list, index):
-    allowance_list.insert(index, user_input)
-    print(f"You added {user_input}")
-
-#==========================
-#Function: Add Expense
-#==========================
-def add_expense(cost, item, cost_list, item_list):
-    print()
-    item_list.insert(index, item)
-    cost_list.insert(index, cost)
-
-#=======================================
-#Function: View All Transanctions
-#=======================================
-def view_transactions(allowance_list, item_list, date_list_expense, date_list_allowance, cost_list):
-    print()
-    print("Allowance added: ")
-    print(f"Date: {date_list_allowance}")
-    print(f"Allowance: {allowance_list}")
-    print()
-    print("Expenses: ")
-    print(f"Date: {date_list_expense}")
-    print(f"Item: {item_list}")
-    print(f"Cost: {cost_list}")
-    print()
-
-#=======================================
-#Function: Summary of All Transanctions
-#=======================================
-def add_summary(total, allowance_total, remainder):
-    print()
-    print(f"Allowance total: {allowance_total}")
-    print()
-    print(f"Expense total: {total}")
-    print()
-
-    remainder = allowance_total - total
-
-    print(f"Remainder: {remainder}")
-    print()
-
-
-# While loop to keep user in the program
-while 1 != 2:
-
-    #Main menu
-    print("="*50, " MAIN MENU ", "="*50)
+allowance = 0
+date_list = []
+money_left = 0
+expense_list = []
+total_expense_added = 0
+total_allowance_added = 0
+while 1 == 1:
+    print("=" * 50, " MAIN MENU ", "=" * 50)
     print("1. Add allowance")
     print("2. Add expense")
     print("3. View transactions")
@@ -86,101 +40,45 @@ while 1 != 2:
 
     print("=" * 113)
 
-    #This asks the user to input their choice
+    # This asks the user to input their choice
     user_input = int(input("Enter your choice: "))
     print()
 
-    #If the user inputs "1" as their choice, the program will ask the user how much money do they want to add into their allowance.
     if user_input == 1:
 
-        user_input = float(input("Add allowance (ex. 50 php add): "))
-        #If the user's input is less than or equal to 0, the input will not be recorded and then bring you back to the main menu.
-        if user_input == 0 or user_input<0:
-            print("Invalid inputs will not be recorded.")
-            print()
-            continue
+        user_input2 = float(input("Add allowance (ex. 50 php add): "))
 
-        #This will add the inputted amount into the total allowance.
-        allowance_total += user_input
+        total_allowance_added += user_input2
+        allowance += user_input2
+        money_left += user_input2
 
-        #This tells the date that the allowance added.
-        from datetime import date
-        
-        current_date = date.today()
-        date = current_date.strftime("%Y-%m-%d")
-        date_list_allowance.insert(index, date)
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            for x in data:
+                x['Allowance_added'] = total_allowance_added
 
-        add_allowance(user_input, allowance_list, index)
+            for x in file:
+                print(x)
 
-    #If the user inputs "2" as their choice, the program will ask the user how much money did they spend.
     elif user_input == 2:
+        user_input2 = float(input(""))
 
-        cost = float(input("Money spent: "))
-    #If the user's input is equal to 0, the input will not be recorded and then bring you back to the main menu.
-        if cost == 0:
-            print("An input of 0 will not be recorded.")
-            print()
-            continue
+        user_input3 = str(input(""))
 
-        #This adds the money you spent to the total money you spent.
-        total += cost
+        if user_input > allowance:
+            print("You are now in debt.")
 
-        #If the total allowance minus the total money spent is less than 0, the program will tell them that they spent too much.
-        if allowance_total - total < 0:
-            total -= cost
-            print("You spent too much!")
-            continue
+        total_expense_added += user_input2
 
-        #The program asks what the user spent their money on
-        item = str(input("What item was the money spent on? "))
+        allowance -= user_input
+        expense_list.append(user_input3)
 
-        #This tells the date of when the money spent.
-        from datetime import date
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            for x in data:
+                x['Expenses_added'] = total_expense_added
 
-        current_date = date.today()
-        date = current_date.strftime("%Y-%m-%d")
-        date_list_expense.insert(index, date)
-
-        add_expense(cost, item, cost_list, item_list)
-        
-    #If the user inputs "3" as their choice, then it will print all the transanctions
-    elif user_input == 3:
-        view_transactions(allowance_list, item_list, date_list_expense, date_list_allowance, cost_list)
-        
-    #If the user inputs "4" as their choice, the program will print the summary of all transanctions.
-    elif user_input == 4:
-        add_summary(total, allowance_total, remainder)
-
-    #If the user inputs "0" as their choice, the program will break/end.
-    elif user_input == 0:
-        break
-
-    #If the user inputs non of the choices, the program will tell them that their input is invalid, and then ask them if they woulf like to go back to the main menu.
-    #Invalid user input    
-    else:
-        print("Invalid input.")
-        print()
-    print()
-
-    return_menu = str(input("Would you like to go back to the main menu? (Yes/No) "))
-
-    #Used to validate yes/no
-    while checker==0:
-        if return_menu.lower() == "yes":
-            checker+=1
-        elif return_menu.lower() == "no":
-            checker+=1
-        else:
-            print("Invalid input.")
-            print("="*113)
-            return_menu = str(input("Would you like to go back to the main menu? (Yes/No) "))
-
-    if return_menu.lower() == "yes":
-        continue
-    else:
-        break
-
-#End program
-print("="*113)
-print("\nThank you for using our program!\n")
-print("="*113)
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            for x in data:
+                x['Expenses_list'] = expense_list
